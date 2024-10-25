3801b5caf1464d59514e200cf8fecb583db2c3fd
Koushik6692 <105788507+Koushik6692@users.noreply.github.com>
2024-10-25 15:09:43 +0530

    made app pwa(app can be installed on windows android ios)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
@@ -7,10 +7,49 @@ import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "GostlyNotes";
+const APP_DEFAULT_TITLE = "Send and recieve anonymus messages!";
+const APP_TITLE_TEMPLATE = "%s - GostlyNotes App";
+const APP_DESCRIPTION = "Speak freely, share boldly";
+
 export const metadata: Metadata = {
-  title: "GhostlyNotes",
-  description: "Send anonymus messages to anyone!",
-  
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
